8680d3b4503dc10414d7e4f8efa970ee00dbdad4
ulito <ulitotech@gmail.com>
2024-05-16 12:13:10 +0300

    Changed ping process and throttling middleware

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker
 from typing import Any, Callable, Dict, Awaitable
 import datetime
 from loguru import logger
+import copy
 
 USERS = {}
 
@@ -42,9 +43,14 @@ class ThrottlingMiddleware(BaseMiddleware):
             usr_id = event.callback_query.message.chat.id
         if usr_id in USERS.keys():
             if abs(upd_date-USERS[usr_id]) > throttling_delta:
-                for k in USERS.keys():
-                    if abs(upd_date - USERS[usr_id]) > use_limit and k != usr_id:
-                        del USERS[k]
+                users_id = copy.deepcopy(set(k for k in USERS.keys()))
+                for k in users_id:
+                    if k in USERS.keys():
+                        try:
+                            if abs(upd_date - USERS[k]) > use_limit and k != usr_id:
+                                del USERS[k]
+                        except Exception:
+                            continue
                 USERS[usr_id] = upd_date
                 result = await handler(event, data)
                 return result
@@ -52,9 +58,14 @@ class ThrottlingMiddleware(BaseMiddleware):
                 USERS[usr_id] = upd_date
                 logger.info(f'Пользователь: {usr_id} пытался уронить сервер!!!')
         else:
-            for k in USERS.keys():
-                if abs(upd_date - USERS[usr_id]) > use_limit and k != usr_id:
-                    del USERS[k]
+            users_id = copy.deepcopy(set(k for k in USERS.keys()))
+            for k in users_id:
+                if k in USERS.keys():
+                    try:
+                        if abs(upd_date - USERS[k]) > use_limit and k != usr_id:
+                            del USERS[k]
+                    except Exception:
+                        continue
             USERS[usr_id] = upd_date
             result = await handler(event, data)
             return result
--- utils/ping_processing.py
+++ utils/ping_processing.py
@@ -3,12 +3,12 @@ import asyncio
 
 async def connection_test(ip: str) -> bool:
     """Пингует ip 5 раз и возвращает состояние устройства"""
-    cmd = f'ping /n 5 {ip}'
-    process = await asyncio.create_subprocess_shell(cmd,
-                                                    stdout=asyncio.subprocess.PIPE,
-                                                    stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await process.communicate()
-    if stdout.decode(encoding='cp866').count('TTL') > 0:
-        return True
-    else:
-        return False
+    cmd = f'ping /n 1 {ip}'
+    for i in range(5):
+        process = await asyncio.create_subprocess_shell(cmd,
+                                                        stdout=asyncio.subprocess.PIPE,
+                                                        stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await process.communicate()
+        if 'TTL' in stdout.decode(encoding='cp866'):
+            return True
+    return False
