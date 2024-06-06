deebb000a968e1972a9403677c728e13d3d49504
ulito <ulitotech@gmail.com>
2024-06-06 11:15:33 +0300

    added changing symbols from 160 to 32

--- utils/sms_processes.py
+++ utils/sms_processes.py
@@ -1,29 +1,23 @@
 import time
 import serial
 
-
-phone = serial.Serial("COM4",  9600)
+phone = serial.Serial("COM3",  9600)
 time.sleep(1)
-while True:
-   cmd = input('-> ')
-   cmd = cmd+'\r\n'
-   phone.write(cmd.encode())
-   time.sleep(1)
-   print(phone.read_all())
+
 print('start')
 cmd = "AT\r\n"
 phone.write(cmd.encode())
-time.sleep(1)
+time.sleep(0.5)
 print(phone.read_all())
 cmd = "AT+CMGF=1\r\n"
 phone.write(cmd.encode())
-time.sleep(1)
+time.sleep(0.5)
 print(phone.read_all())
-cmd = 'AT+CMGS="number"\r\n'
+cmd = 'AT+CMGS="+7990"\r\n'
 phone.write(cmd.encode())
 time.sleep(0.5)
 print(phone.read_all())
-sms = 'yo\r\n'
+sms = 'test\r\n'
 phone.write(sms.encode())
 time.sleep(0.5)
 print(phone.read_all())
