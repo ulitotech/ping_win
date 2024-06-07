5e99d21db3aa8d2cafbff87ca359f8c191bd1069
ulito <ulitotech@gmail.com>
2024-06-07 15:02:45 +0300

    created ability for sending sms via gsm

---
+++ utils/working_with_task.py
@@ -0,0 +1,13 @@
+from database.orm_query import change_task_status, get_task
+from utils.sms_processes import send_sms_via_gsm
+from loguru import logger
+
+
+async def check_task_table():
+    task = await get_task()
+    if task is not None and task.status == 0:
+        logger.info(f'В списке есть задание на отправку СМС через модуль на номер {task.phone_number}')
+        await change_task_status(1)
+        logger.info(f'Задание по отправке СМС на номер {task.phone_number} принято в работу')
+        await send_sms_via_gsm(task.text, task.phone_number)
+        logger.info(f'Идет отправка сообщения на номер {task.phone_number}...')
