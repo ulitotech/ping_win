e0d15656f5d0372cb75961214537300ce297fc0a
ulito <ulitotech@gmail.com>
2024-07-31 09:42:23 +0300

    updated ping command

--- utils/ping_processing.py
+++ utils/ping_processing.py
@@ -6,7 +6,8 @@ import datetime
 async def connection_test(ip: str) -> bool:
     """Пингует ip несколько раз и возвращает состояние устройства"""
     if 'linux' in platform:
-        cmd = ['ping', '-c', '130', '-W', '5' f'{ip}']
+        print('это линуха')
+        cmd = ['ping', '-c', '130', '-W', '5', '-O', f'{ip}']
     else:
         cmd = ['ping', '-n', '130', '-w', '5000', f'{ip}']
     process = await asyncio.create_subprocess_exec(*cmd,
@@ -18,8 +19,10 @@ async def connection_test(ip: str) -> bool:
         line = await process.stdout.readline()
         if line.decode(encoding='cp866'):
             decoded_result = (line.decode(encoding='cp866'))
+            print(decoded_result)
             if any(ans in decoded_result for ans in ['ttl', 'TTL']):
                 process._transport.close()
                 return True
+        await asyncio.sleep(1)
     process._transport.close()
     return False
